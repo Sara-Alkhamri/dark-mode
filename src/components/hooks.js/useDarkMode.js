cd5a2a50b7f11e98ffe33798ae2488269663a49c
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-04 14:12:33 -0700

    Created useDarkMode function. Step two completed

---
+++ src/components/hooks.js/useDarkMode.js
@@ -0,0 +1,20 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (initialValue) => {
+   
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue);
+    console.log(darkMode);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add('dark-mode');
+        }
+        else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
